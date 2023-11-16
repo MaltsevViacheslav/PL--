1f9d867958a102b7fd22f32fc5506e3831cf6084
MaltsevViacheslav <145259665+MaltsevViacheslav@users.noreply.github.com>
2023-11-16 22:24:49 +0300

    Update 2.py

--- Pr8/2.py
+++ Pr8/2.py
@@ -1,25 +1,26 @@
 #1
-n = 3
 A = [[1, 4, 5], [8, 1, 1], [1, 5, 4]]
+n = 3
 s = 0
-for i in range(n):
-    s += A[0][i]
-b = "да, является"
+for j in range(n):
+    s += A[0][j]
+    b = "да, является"
 for i in range(n):
     s1 = 0
     s2 = 0
     for j in range(n):
         s1 += A[i][j]
-        s2 += A[j][i]
+        s2 += A[i][j]
     if s1 != s or s2 != s:
         b = "нет, не является"
-        break
 print(b)
 
 #2
 n = 3
 m = 4
-A = [[1, 2, 3, 4], [5, 6, 7, 8], [9,10,11,12]]
+A = [[1, 2, 3, 4],
+     [5, 6, 7, 8],
+     [9, 10, 11, 12]]
 for i in range(n):
     A1 = A[i][0]
     A[i][0] = A[i][m-1]
