6add3c1b0399a061c75c25413e54be31130e1bbd
MaltsevViacheslav <145259665+MaltsevViacheslav@users.noreply.github.com>
2023-11-16 21:46:36 +0300

    Update 3.py

--- Pr7/3.py
+++ Pr7/3.py
@@ -1,14 +1,13 @@
 #1
-import math
 def gip(x, y):
-    return math.sqrt(x ** 2 + y ** 2)
+    return (x ** 2 + y ** 2)**(0.5)
 
 a1, b1 = map(float, input("Катеты 1-го треугольника a1, b1 = ").split())
 a2, b2 = map(float, input("Катеты 2-го треугольника a2, b2 = ").split())
 c1 = gip(a1, b1)
-print(c1)
+print("Гипотенуза 1-го треугольника =", c1)
 c2 = gip(a2, b2)
-print(c2)
+print("Гипотенуза 2-го треугольника =", c2)
 if c1 == c2:
     print("Гипотенузы равны")
 if c1 > c2:
@@ -17,7 +16,7 @@ else:
     print("Гипотенуза 2-го треугольника больше")
 
 #2
-s = input("").split(" ")
+s = input().split()
 for i in range(len(s)):
     s[i] = "".join(sorted(s[i]))
 print(" ".join(s))
