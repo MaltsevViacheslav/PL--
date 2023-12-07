fb54b9c13ee2718fa97b0fae3897a62c1b24591a
MaltsevViacheslav <145259665+MaltsevViacheslav@users.noreply.github.com>
2023-12-08 00:05:47 +0300

    Add files via upload

---
+++ Pr10/Pr10.2/Pr10.2.py
@@ -0,0 +1,24 @@
+file1 = open("Мальцев В. М._УБ-32_vvod2.txt")
+file2 = open("Мальцев В. М._УБ-32_vivod2.txt", "w", encoding= "utf-8")
+A = []
+for i in file1:
+    elem = []
+    for j in i.strip().split():
+        elem.append(int(j))
+    A.append(elem)
+n = 3
+s = 0
+for j in range(n):
+    s += A[0][j]
+for i in range(n):
+    s1 = 0
+    s2 = 0
+    for j in range(n):
+        s1 += A[i][j]
+        s2 += A[i][j]
+if s1 != s or s2 != s:
+    file2.write("Нет, не является")
+else:
+    file2.write("Да, является")
+file1.close()
+file2.close()
