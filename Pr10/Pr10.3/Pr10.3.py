1d8b86647903c72364d20b5acc58fb6345c25714
MaltsevViacheslav <145259665+MaltsevViacheslav@users.noreply.github.com>
2023-12-08 10:48:47 +0300

    Add files via upload

---
+++ Pr10/Pr10.3/Pr10.3.py
@@ -0,0 +1,19 @@
+file1 = open("Мальцев В. М._УБ-32_vvod3.txt")
+file2 = open("Мальцев В. М._УБ-32_vivod3.txt", "w", encoding= "utf-8")
+n = 3
+m = 4
+A = []
+for i in file1:
+    list= []
+    for j in i.strip().split():
+        list.append(int(j))
+    A.append(list)
+for i in range(n):
+    A1 = A[i][0]
+    A[i][0] = A[i][m-1]
+    A[i][m-1] = A1
+file2.write(str(A))
+file1.close()
+file2.close()
+
+
